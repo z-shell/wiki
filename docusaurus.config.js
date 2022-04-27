5ebabb814707d604604463a84c60fca3e2645d48
Salvydas Lukosius <sall@w-ss.io>
2022-04-27 04:20:31 +0100

    lockfile

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,7 +71,7 @@ const config = {
     ],
   ],
   scripts: [{defer: 'true', src: 'https://space.ss-o.workers.dev/fa/js/all.js'}],
-  clientModules: [require.resolve('./src/fa/js/all.js')],
+  clientModules: [require.resolve('./src/fa/css/all.css')],
   presets: [
     [
       'classic',
