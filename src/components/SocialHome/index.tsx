9ac46a61391937e8c99894cd055eec28f2d334f7
Salvydas Lukosius <sall@w-ss.io>
2022-04-30 11:37:35 +0100

    Update index.tsx

--- src/components/SocialHome/index.tsx
+++ src/components/SocialHome/index.tsx
@@ -8,28 +8,22 @@ export default function SocialHome() {
     <main className={SocialHome}>
       <div className="container" align="center">
         <Link href="https://twitter.com/zshell_zi">
-          <p>
             <img
-              loading="lazy"
               alt="An Awesome Image"
               width="80%"
               height="auto"
-              src="https://raw.githubusercontent.com/z-shell/.github/main/metrics/plugin/tweets/tweets.svg"
+              src="https://github.zshell.dev/.github/plugin/tweets/tweets.svg"
               alt="Recent Tweets"
             />
-          </p>
         </Link>
         <Link href="https://dev.to/tag/zsh">
-          <p>
             <img
-              loading="lazy"
               alt="An Awesome Image"
               width="80%"
               height="auto"
-              src="https://raw.githubusercontent.com/z-shell/.github/main/metrics/plugin/rss/dev.tag.zsh.rss.svg"
+              src="https://github.zshell.dev/.github/plugin/rss/dev.tag.zsh.rss.svg"
               alt="dev.to/z-shell"
             />
-          </p>
         </Link>
       </div>
     </main>
