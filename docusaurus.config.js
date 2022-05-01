a1383728fbc3f36e63a19414c38f8d8dae07c7e1
Salvydas Lukosius <sall@w-ss.io>
2022-05-01 18:26:47 +0100

    min.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,16 @@ const config = {
   organizationName: 'z-shell',
   staticDirectories: ['static'],
   i18n: {defaultLocale: 'en', locales: ['en', 'ja', 'zh-Hans']},
-  scripts: [{defer: true, src: 'https://cdn.digitalclouds.dev/fa/js/all.min.js'}],
+  scripts: [
+    {
+      defer: true,
+      src: 'https://cdn.digitalclouds.dev/fa/js/brands.min.js',
+      defer: true,
+      src: 'https://cdn.digitalclouds.dev/fa/js/solid.min.js',
+      defer: true,
+      src: 'https://cdn.digitalclouds.dev/fa/js/fontawesome.min.js',
+    },
+  ],
   plugins: [
     ['ideal-image', {max: 960, min: 480, disableInDev: false}],
     [
