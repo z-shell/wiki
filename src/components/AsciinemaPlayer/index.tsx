d07cbb166cb63cc4b263397df9833c473ce326a3
Salvydas Lukosius <sall@w-ss.io>
2022-05-08 15:42:06 +0100

    workspace(loadable)

--- src/components/AsciinemaPlayer/index.tsx
+++ src/components/AsciinemaPlayer/index.tsx
@@ -22,17 +22,17 @@ type AsciinemaPlayerProps = {
 };
 
 const AsciinemaPlayer: React.FC<AsciinemaPlayerProps> = ({
-    src,
-    ...asciinemaOptions
+  src,
+  ...asciinemaOptions
 }) => {
-    const ref = useRef<HTMLDivElement>(null);
+  const ref = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        const currentRef = ref.current;
-        AsciinemaPlayerLibrary.create(src, currentRef, asciinemaOptions);
-    }, [src]);
+  useEffect(() => {
+    const currentRef = ref.current;
+    AsciinemaPlayerLibrary.create(src, currentRef, asciinemaOptions);
+  }, [src]);
 
-    return <div className={styles.videoContainer} id="player" ref={ref} />;
+  return <div className={styles.videoContainer} id="player" ref={ref} />;
 };
 
 export default AsciinemaPlayer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,14 @@ import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import loadable from '@loadable/component';
-import HomepageFeatures from '@site/src/components/HomepageFeatures'
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import styles from './styles.module.css';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
-  const AsciinemaPlayer = loadable(() => import('@site/src/components/AsciinemaPlayer'));
+  const AsciinemaPlayer = loadable(
+    () => import('@site/src/components/AsciinemaPlayer'),
+  );
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
