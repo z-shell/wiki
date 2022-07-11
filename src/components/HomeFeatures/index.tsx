23cfd4da62858a2a52e65b31c46a9abf13729aec
Salvydas Lukosius <sall@w-ss.io>
2022-07-11 06:36:12 +0100

    modified:   src/components/HomeFeatures/index.tsx 	modified:   src/components/HomeFeatures/styles.module.css 	modified:   src/css/custom.css 	modified:   src/pages/index.module.css 	modified:   src/pages/index.tsx

--- src/components/HomeFeatures/index.tsx
+++ src/components/HomeFeatures/index.tsx
@@ -3,31 +3,25 @@ import clsx from 'clsx';
 import Translate, { translate } from '@docusaurus/Translate';
 import styles from './styles.module.css';
 
-/** type FeatureItem = {
-  title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
-  description: JSX.Element;
-}; */
-
 const turboSvg = () => (
-  <span className="fa-8x">
+  <span className="fa-6x">
     <i className="fa-solid fa-forward" />
   </span>
 );
 
 const statsSvg = () => (
-  <span className="fa-10x">
+  <span className="fa-8x">
     <i className="fa-solid fa-ranking-star" />
   </span>
 );
 
 const createSvg = () => (
-  <span className="fa-8x">
+  <span className="fa-6x">
     <i className="fa-solid fa-layer-group" />
   </span>
 );
 
-const FeatureList = [
+const features = [
   {
     title: translate({
       id: 'homepage.feature1.title',
@@ -86,15 +80,15 @@ function HomeFeatures(): JSX.Element {
     <section className={styles.features}>
       <div className="container">
         <div className="row">
-          {FeatureList.map(({ title, Svg, description }, idx) => (
+          {features.map(({ title, Svg, description }, idx) => (
             <div key={idx} className={clsx('col col--4', styles.features)}>
-              <div className="text--center">
-                <Svg role="img" alt={title} />
-              </div>
-              <div className="text--center padding-horiz--md">
-                <h3>{title}</h3>
-                <p>{description}</p>
-              </div>
+              {Svg && (
+                <div className={styles.featureimage}>
+                  <Svg role="img" alt={title} />
+                </div>
+              )}
+              <h3>{title}</h3>
+              <p>{description}</p>
             </div>
           ))}
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,32 +12,20 @@ const AsciinemaPlayer = loadable(
     import(/* webpackPrefetch: true */ '@site/src/components/AsciinemaPlayer')
 );
 
-function HomeHeader(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.herobanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-      </div>
-    </header>
-  );
-}
-
 function FeaturesContainer(): JSX.Element {
   return (
     <section className={styles.videocontainer}>
       <div className="container">
-        <div>
-          <h2>
-            <Translate
-              id="homepage.video.heading.1"
-              description="The homepage video conatainer heading 1"
-            >
-              ⚡ Fast and feature-rich
-            </Translate>
-          </h2>
+        <div className="col">
           <div className={styles.asciicasts}>
+            <h2>
+              <Translate
+                id="homepage.video.heading.1"
+                description="The homepage video conatainer heading 1"
+              >
+                ⚡ Fast and feature-rich
+              </Translate>
+            </h2>
             <AsciinemaPlayer
               src="https://asciinema.org/a/459358.cast"
               /* poster="npt:0:30" */
@@ -48,22 +36,18 @@ function FeaturesContainer(): JSX.Element {
               preload
             />
           </div>
-        </div>
-        <div>
           <div className={styles.features}>
             <HomeFeatures />
           </div>
-        </div>
-        <div>
-          <h2>
-            <Translate
-              id="homepage.video.heading.2"
-              description="The homepage video container heading 2"
-            >
-              ✨ Neat and flexible
-            </Translate>
-          </h2>
           <div className={styles.asciicasts}>
+            <h2>
+              <Translate
+                id="homepage.video.heading.2"
+                description="The homepage video container heading 2"
+              >
+                ✨ Neat and flexible
+              </Translate>
+            </h2>
             <AsciinemaPlayer
               /* poster="npt:2:34" */
               src="https://asciinema.org/a/497831.cast"
@@ -84,7 +68,12 @@ function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout title={siteConfig.tagline}>
-      <HomeHeader />
+      <header className={clsx('hero hero--primary', styles.herobanner)}>
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+        </div>
+      </header>
       <main>
         <FeaturesContainer />
       </main>
