5d66a71193da602622c676d0395a26e8589863b9
0xMRTT <0xMRTT@tuta.io>
2022-07-11 09:55:34 +0200

    Add CTA on homepage (#675)
    * Add CTA on homepage
    
    * Add CTA on homepage (fix trunk issues)
    
    * Update index.tsx
    
    Signed-off-by: Salvydas Lukosius <sall@w-ss.io>
    
    Co-authored-by: Salvydas Lukosius <sall@w-ss.io>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import clsx from 'clsx';
 import baseLoad from '@loadable/component';
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Translate from '@docusaurus/Translate';
+import Translate, { translate } from '@docusaurus/Translate';
+import Link from '@docusaurus/Link';
 import styles from './index.module.css';
 
 function loadable(func) {
@@ -69,15 +69,63 @@ function FeaturesContainer(): JSX.Element {
   );
 }
 
+function HeroBanner() {
+  return (
+    <div className={styles.hero} data-theme="dark">
+      <div className={styles.heroInner}>
+        <h1 className={styles.heroProjectTagline}>
+          <img
+            alt={translate({ message: 'ZI logo' })}
+            className={styles.heroLogo}
+            src="/img/logo.svg"
+            width="200"
+            height="200"
+          />
+          <span
+            className={styles.heroTitleTextHtml}
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: translate({
+                id: 'homepage.hero.title',
+                message:
+                  'A <b>Swiss Army</b> Knife  for <b>Zsh</b> unix <b>Shell</b>',
+                description:
+                  'Home page hero title, can contain simple html tags',
+              }),
+            }}
+          />
+        </h1>
+        <div className={styles.indexCtas}>
+          <Link
+            className="button button--primary"
+            to="/docs/getting_started/installation"
+          >
+            <Translate>Get Started</Translate>
+          </Link>
+          <Link className="button button--secondary" to="/community/intro">
+            <Translate>Community</Translate>
+          </Link>
+          <span className={styles.indexCtasGitHubButtonWrapper}>
+            <iframe
+              className={styles.indexCtasGitHubButton}
+              src="https://ghbtns.com/github-btn.html?user=z-shell&amp;repo=zi&amp;type=star&amp;count=true&amp;size=large"
+              width={160}
+              height={30}
+              title="GitHub Stars"
+            />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout title={siteConfig.tagline}>
-      <header className={clsx('hero hero--primary', styles.herobanner)}>
-        <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-        </div>
+      <header>
+        <HeroBanner />
       </header>
       <main>
         <FeaturesContainer />
