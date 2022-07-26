06eb02f5a1392eb52001ab0e3a6e9feeb6a66363
Salvydas Lukosius <sall@w-ss.io>
2022-07-26 20:54:12 +0100

    modified:   src/pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,10 @@
 /** @format */
 
 import React from 'react';
-import Loadable from 'react-loadable';
-import Loading from '@site/src/components/Spinner';
 import Layout from '@theme/Layout';
+import HomeBanner from '@site/src/components/HomeBanner';
 import HomeFeatures from '@site/src/components/HomeFeatures';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-
-const HomeBanner = Loadable({
-	loader: () => import('@site/src/components/HomeBanner'),
-	loading: Loading,
-});
 
 function Home(): JSX.Element {
 	const { siteConfig } = useDocusaurusContext();
