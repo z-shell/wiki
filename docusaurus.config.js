c028c6dd29937b55e00483aa1f54f489e015337f
Salvydas Lukosius <sall@w-ss.io>
2022-07-26 21:19:55 +0100

    modified:   docusaurus.config.js 	modified:   src/css/custom.css

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
 	projectName: 'wiki',
 	organizationName: 'z-shell',
 	staticDirectories: ['static'],
-	stylesheets: ['/fa/css/all.min.css'],
+	stylesheets: ['/assets/fa/css/all.min.css'],
 	i18n: {
 		defaultLocale: 'en',
 		locales: ['en', 'fr', 'ja', 'zh-Hans'],
