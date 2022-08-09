f97ffe68f02ce4e5f67ebb210f51c3ee70aad9bf
Salvydas Lukosius <sall@w-ss.io>
2022-08-09 21:28:34 +0100

    modified:   pnpm-lock.yaml 	modified:   src/components/Spinner/index.tsx

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -5,21 +5,22 @@ import React, { useState, CSSProperties } from "react";
 import ClipLoader from "react-spinners/ClipLoader";
 
 const override: CSSProperties = {
+  color: "var(--ifm-color-primary)",
   display: "block",
   margin: "0 auto",
-  color: "var(--ifm-color-primary)",
 };
 
 export default function Spinner(): JSX.Element {
   const [loading] = useState(true);
-  const [color] = useState("#ffffff");
+  const [color] = useState("#00ffcc");
   return (
     <div className='sweet-loading'>
       <ClipLoader
+        size={220}
         color={color}
         loading={loading}
+        speedMultiplier={0.8}
         cssOverride={override}
-        size={220}
       />
     </div>
   );
