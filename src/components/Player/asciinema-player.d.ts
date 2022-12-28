439b765c3ca19623b0aebb24bb488a4b74201904
Salvydas Lukosius <sall@w-ss.io>
2022-12-28 07:48:06 +0000

    player 	modified:   pnpm-lock.yaml 	new file:   src/components/Player/asciinema-player.d.ts 	modified:   src/components/Player/index.tsx 	modified:   src/css/custom.css

---
+++ src/components/Player/asciinema-player.d.ts
@@ -0,0 +1,22 @@
+declare module "asciinema-player" {
+  export function create(
+    src: string,
+    element: HTMLElement | null,
+    opts: {
+      cols?: number;
+      rows?: number;
+      autoPlay?: boolean;
+      preload?: boolean;
+      loop?: boolean | number;
+      startAt?: number | string;
+      speed?: number;
+      idleTimeLimit?: number;
+      theme?: string;
+      poster?: string;
+      fit?: string;
+      terminalLineHeight?: number;
+      terminalFontFamily?: string;
+      terminalFontSize?: string;
+    }
+  );
+}
