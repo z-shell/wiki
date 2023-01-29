31d4ddd4a057274f82d41bca517c1ec6f9381e00
Salvydas Lukosius <sall@w-ss.io>
2023-01-29 00:55:19 +0000

    modified:   src/components/Player/index.tsx

--- src/components/Player/asciinema-player.d.ts
+++ src/components/Player/asciinema-player.d.ts
@@ -1,22 +1,20 @@
 declare module "asciinema-player" {
-  export function create(
-    src: string,
-    element: HTMLElement | null,
-    opts: {
-      cols?: number;
-      rows?: number;
-      autoPlay?: boolean;
-      preload?: boolean;
-      loop?: boolean | number;
-      startAt?: number | string;
-      speed?: number;
-      idleTimeLimit?: number;
-      theme?: string;
-      poster?: string;
-      fit?: boolean | string;
-      terminalLineHeight?: number;
-      terminalFontFamily?: string;
-      terminalFontSize?: string;
-    }
-  );
+  export function create(src: string, element: HTMLElement | null, options: PlayerOptions);
+
+  export interface PlayerOptions {
+    cols?: number;
+    rows?: number;
+    autoPlay?: boolean;
+    preload?: boolean;
+    loop?: boolean | number;
+    startAt?: number | string;
+    speed?: number;
+    idleTimeLimit?: number;
+    theme?: string;
+    poster?: string;
+    fit?: boolean | string;
+    terminalLineHeight?: number;
+    terminalFontFamily?: string;
+    terminalFontSize?: string;
+  }
 }
--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -2,27 +2,10 @@
 
 import React, {useEffect, useRef, useState} from "react";
 import Spinner from "@site/src/components/Spinner";
+import type {PlayerOptions} from "asciinema-player";
 import "asciinema-player/dist/bundle/asciinema-player.css";
 
-interface PlayerConfig {
-  src: string;
-  cols?: number;
-  rows?: number;
-  autoPlay?: boolean;
-  preload?: boolean;
-  loop?: boolean | number;
-  startAt?: number | string;
-  speed?: number;
-  idleTimeLimit?: number;
-  theme?: string;
-  poster?: string;
-  fit?: boolean | string;
-  terminalLineHeight?: number;
-  terminalFontFamily?: string;
-  terminalFontSize?: string;
-}
-
-export default function Player(props: PlayerConfig): JSX.Element {
+export default function Player(props: {src: string} & PlayerOptions): JSX.Element {
   const {src, ...opts} = props;
   const element = useRef<HTMLDivElement>(null);
   const [player, setPlayer] = useState<typeof import("asciinema-player")>();
