2f5a1cbff469bbb3f3aab83adcfb2ce43fb15b24
Salvydas Lukosius <ss-o@users.noreply.github.com>
2026-05-01 11:10:44 +0100

    chore(deps): update Docusaurus and related dependencies
    - Bump Docusaurus packages to version 3.10.1
    - Update @cloudflare/workers-types to version 4.20260501.1
    - Update styles and remove unnecessary CSS properties
    - Add Content Security Policy to _headers
    - Introduce SearchBarWrapper for accessibility improvements
    
    Signed-off-by: Salvydas Lukosius <ss-o@users.noreply.github.com>

--- src/theme/MDXContent.tsx
+++ src/theme/MDXContent.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import Head from "@docusaurus/Head";
+import React, {useEffect} from "react";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import MDXContent from "@theme-original/MDXContent";
 
@@ -10,15 +9,16 @@ type SiteCustomFields = {
 export default function MDXContentWrapper(props: React.ComponentProps<typeof MDXContent>): React.JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   const customFields = siteConfig.customFields as SiteCustomFields;
+  const src = customFields.fontAwesomeScript;
 
-  return (
-    <>
-      {customFields.fontAwesomeScript ? (
-        <Head>
-          <script src={customFields.fontAwesomeScript} crossOrigin="anonymous" defer />
-        </Head>
-      ) : null}
-      <MDXContent {...props} />
-    </>
-  );
+  useEffect(() => {
+    if (src && !document.querySelector(`script[src="${src}"]`)) {
+      const script = document.createElement("script");
+      script.src = src;
+      script.crossOrigin = "anonymous";
+      document.head.appendChild(script);
+    }
+  }, [src]);
+
+  return <MDXContent {...props} />;
 }
